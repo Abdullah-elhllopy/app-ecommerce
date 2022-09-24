dcdfcc111064e57e818d1a54e11f3cc4ee72ab6b
Abdallah Ahmed <abdullahahmed199654@gmail.com>
2022-09-25 00:48:59 +0200

    first commit

--- App.js
+++ App.js
@@ -1,41 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from "@react-navigation/native";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import Login from "./screens/Login";
-import Home from './screens/Home';
-import SignUp from './screens/SignUp';
-import Product from './screens/Product';
-import Cart from './screens/Cart';
-import Checkout from './screens/Checkout';
-import Tabs from './navigation/Tabs';
+import { useFonts } from 'expo-font';
+import AppNavigator from './src/navigation';
+import {I18nManager} from "react-native";
 
 export default function App() {
-    const stack = createNativeStackNavigator();
-    return (
-        <View style={styles.container}>
-            <NavigationContainer>
-
-                <stack.Navigator initialRouteName="Login">
-                    <stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-                    <stack.Screen name="Sign Up" component={SignUp} options={{headerShown: false}}/>
-                    <stack.Screen name="Product" component={Product} options={{headerShown: false}}/>
-                    <stack.Screen name="Cart" component={Cart} options={{headerShown: false}}/>
-                    <stack.Screen name="Checkout" component={Checkout} options={{headerShown: false}}/>
-
-                    {/* Bottom Navigation */}
-                    <stack.Screen name="Main" component={Tabs} options={{headerShown: false}}/>
-                </stack.Navigator>
-
-            </NavigationContainer>
-        </View>
-    );
+    const [fontsLoaded] = useFonts({
+        'Khayal': require('./src/assets/fonts/KhayalRegular.ttf'),
+        'cairo':require('./src/assets/fonts/Cairo-Regular.ttf'),
+        'cairoBold':require('./src/assets/fonts/Cairo-Bold.ttf'),
+        'cairoIntail' :require('./src/assets/fonts/Cairo-VariableFont_wght.ttf'),
+    });
+    if (!fontsLoaded) {
+        return null;
+    }
+    const rtl = () => {
+        I18nManager.allowRTL(true);
+        I18nManager.forceRTL(true);
+    };
+    rtl()
+    return (<AppNavigator />);
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
 
 
